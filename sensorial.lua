bdbd6ddbe823b61a526ca2d9a376822b6ea608a4
Alan Livio <alan@telemidia.puc-rio.br>
2016-06-28 18:49:50 -0300

    maint: Clear canvas before draw in NClua.

--- sensorial.lua
+++ sensorial.lua
@@ -8,48 +8,41 @@ local tonumber = tonumber
 
 local canvas = canvas
 local event = event
-local http = assert (require 'http')
+local http = assert(require 'http')
 _ENV = nil
 
 local api_uri = 'http://139.82.95.37:8080/api/'
-local FT_COLOR = 'yellow'
-canvas:attrColor (1,1,1)
-canvas:clear ()
-canvas:attrColor (FT_COLOR)
+
+local function myDrawText( myText )
+  canvas:attrColor(1,1,1)
+  canvas:clear()
+  canvas:attrColor('yellow')
+  canvas:drawText(0, 0, myText)
+  canvas:flush()
+end
 
 local function actuator_request( uri )
       -- fetch URI.
       local headers = {}
       local body = ''
-      local status, code, headers, body = http.put (uri, headers, body)
+      local status, code, headers, body = http.put(uri, headers, body)
       if status == false then
-         canvas:drawText (0, 0, 'error:' ..code)
-         canvas:flush ()
+         myDrawText('error:'..code)
       else
-         canvas:drawText (0, 0, body)
-         canvas:flush ()
+         myDrawText(body)
       end
 end
 
--- event.register (
---    function ()
---        print ('---------------------->ncl,attribution,start,light_toogle')
---        http.execute (actuator_request, api_uri..'light')
---    end,
---    {class='ncl', type='presentation', action='start'}
--- )
-
-
 -- attribution callbacks
-event.register (
-  function (evt)
-    print ('---------------------->ncl,attribution,start'.. evt.name)
+event.register(
+  function(evt)
+    print('---------------------->ncl,attribution,start'.. evt.name)
     if evt.name == 'light_toogle' then
-      http.execute (actuator_request, api_uri..'light')
+      http.execute(actuator_request, api_uri..'light')
     elseif evt.name == 'air_toggle' then
-      http.execute (actuator_request, api_uri..'air')
+      http.execute(actuator_request, api_uri..'air')
     elseif evt.name == 'smell_toggle' then
-      http.execute (actuator_request, api_uri..'smell')
+      http.execute(actuator_request, api_uri..'smell')
     end
   end,
   {class='ncl', type='attribution', action='start'}
