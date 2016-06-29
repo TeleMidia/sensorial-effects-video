66d05f0b01023266a4825ad9813347e55c612660
Alan Livio <alan@telemidia.puc-rio.br>
2016-06-28 22:18:54 -0300

    maint: Use set(var=on) in onBeginSet of air and smell videos. NClua onAttribution test var==on and user==fair to better perception of air and smell actuator.

--- sensorial.lua
+++ sensorial.lua
@@ -5,13 +5,13 @@ local os = os
 local pairs = pairs
 local print = print
 local tonumber = tonumber
-
 local canvas = canvas
 local event = event
 local http = assert(require 'http')
 _ENV = nil
 
 local api_uri = 'http://139.82.95.37:8080/api/'
+local user_position
 
 local function myDrawText( myText )
   canvas:attrColor(1,1,1)
@@ -36,13 +36,25 @@ end
 -- attribution callbacks
 event.register(
   function(evt)
-    print('---------------------->ncl,attribution,start'.. evt.name)
     if evt.name == 'light_toogle' then
       http.execute(actuator_request, api_uri..'light')
-    elseif evt.name == 'air_toggle' then
+    elseif(evt.name == 'air_toggle' and evt.value == 'on' and user_position == 'close') then
+      local function myFunc1()
+        http.execute(actuator_request, api_uri..'air')
+      end
+      event.timer(2000,myFunc1)
+    elseif(evt.name == 'air_toggle') then
       http.execute(actuator_request, api_uri..'air')
-    elseif evt.name == 'smell_toggle' then
+    elseif(evt.name == 'smell_toggle' and evt.value == 'on' and user_position == 'close') then
+      local function myFunc1()
+        http.execute(actuator_request, api_uri..'smell')
+      end
+      event.timer(2000,myFunc1)
+    elseif(evt.name == 'smell_toggle') then
       http.execute(actuator_request, api_uri..'smell')
+    elseif evt.name == 'user_position' then
+      user_position = evt.value
+      myDrawText('user_position='..user_position)
     end
   end,
   {class='ncl', type='attribution', action='start'}
